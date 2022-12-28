59cbad5956126fd6146d7386ab41111152c23a4a
Alexander Getahun Tadese <90861762+MilkesaMerry@users.noreply.github.com>
2022-12-28 18:17:06 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,8 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="theme-compiled">
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9909058672380486"
+     crossorigin="anonymous"></script>
         <Head />
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
